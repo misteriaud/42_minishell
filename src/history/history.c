2553bebc2f72c277faafe320b86ccdb8c60f55a5
123Untitled <123untitled@pm.me>
2022-03-26 21:51:34 +0100

    remove gnl implementation

--- src/history/history.c
+++ src/history/history.c
@@ -6,7 +6,7 @@
 /*   By: artblin <artblin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 18:53:26 by artblin           #+#    #+#             */
-/*   Updated: 2022/03/26 21:17:33 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/26 21:41:05 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,32 +26,6 @@ int	init_history(t_ctx *ctx)
 	if (ctx->fd_history < 0)
 		return (OPEN_ERROR);
 
-
-	t_hst	*new;
-	char	*line;
-
-	line = get_next_line(ctx->fd_history);
-	while (line)
-	{
-		if (xmalloc(&new, sizeof(t_hst), HISTORY_GROUP))
-			return (MEMORY_ERROR);
-		new->cmd.str = line;
-		new->next = ctx->history;
-		ctx->history = new;
-		line = get_next_line(ctx->fd_history);
-	}
-
-
-
-	// debug !!!
-	t_hst *elm = ctx->history;
-	printf("run hst_lst\n");
-	while (elm)
-	{
-		printf("-> %s", elm->cmd.str);
-		elm = elm->next;
-	}
-	printf("end run ///\n");
 	return (NO_ERROR);
 }
 
