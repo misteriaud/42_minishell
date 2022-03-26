982824261ae232f885a47f40b23e4b831a6f0ee3
123Untitled <123untitled@pm.me>
2022-03-26 20:06:49 +0100

    update include

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,15 +6,16 @@
 /*   By: mriaud <mriaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 17:57:43 by mriaud            #+#    #+#             */
-/*   Updated: 2022/03/26 18:51:19 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/26 19:07:19 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <history.h>
+# include <history.h>
 # include <struct.h>
+# include <memory.h>
 
 # include <stdio.h>
 # include <unistd.h>
