045b55326d513db1387e42f26ccf7b256f47440e
123Untitled <123untitled@pm.me>
2022-03-26 21:51:06 +0100

    add error enum

--- inc/struct.h
+++ inc/struct.h
@@ -6,12 +6,23 @@
 /*   By: artblin <artblin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 19:56:33 by artblin           #+#    #+#             */
-/*   Updated: 2022/03/26 20:09:16 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/26 21:48:31 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 #define STRUCT_H
+
+typedef enum e_err
+{
+	NO_ERROR = 0,
+	MEMORY_ERROR = 0x10,
+	OPEN_ERROR = 0x20,
+	READ_ERROR = 0x30,
+	OVERFLOW_ERROR = 0x40,
+	ARG_ERROR = 0x50,
+	OPTION_ERROR = 0x60,
+}	t_err;
 
 typedef struct s_str
 {
