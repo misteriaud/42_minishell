be9f84b9e16a4ab57044ca78062503f70950569f
123Untitled <123untitled@pm.me>
2022-03-26 20:05:36 +0100

    add history functions

--- src/history.c
+++ src/history.c
@@ -1,18 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   history.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artblin <artblin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/26 18:53:26 by artblin           #+#    #+#             */
+/*   Updated: 2022/03/26 20:00:42 by artblin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minishell.h>
 
-int	history()
-{
-	if (access(".minishell_history", F_OK))
-	{
-		open(".minishell_history", O_RDWR | O_APPEND | O_CREAT, 0777);
-		write(1, "\U0001F4AD ", 5);
-		write(1, "Creating history file :P\n", 25);
-	}
-	return (0);
-}
+#define NO_ERROR			0
+#define OPEN_ERROR			122
+#define HISTORY_GROUP		130
+#define MEMORY_ERROR		287
+#define HISTORY_FILE_NAME	".minishell_history"
 
+int	init_history(t_ctx *ctx)
+{
+	ctx->fd_history = open(".minishell_history",
+			O_RDWR | O_CREAT | O_SYMLINK, 0777);
+	if (ctx->fd_history < 0)
+		return (OPEN_ERROR);
+	return (NO_ERROR);
+}
 
 int	history_parsing()
 {
 	return (0);
+}
+
+int	add_to_history(t_ctx *ctx, t_str cmd)
+{
+	t_hst	*new;
+
+	write(ctx->fd_history, "\n", 1);
+	write(ctx->fd_history, cmd.str, cmd.len);
+
+	if (xmalloc(&new, sizeof(t_hst), HISTORY_GROUP))
+		return (MEMORY_ERROR);
+	new->cmd = cmd;
+	new->next = ctx->history;
+	ctx->history = new;
+	return (NO_ERROR);
 }
