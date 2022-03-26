1f554f618d188c49deb928f3dc31f6377cd7e8c1
123Untitled <123untitled@pm.me>
2022-03-26 18:37:41 +0100

    struct modif

--- inc/struct.h
+++ inc/struct.h
@@ -6,25 +6,26 @@
 /*   By: artblin <artblin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 19:56:33 by artblin           #+#    #+#             */
-/*   Updated: 2022/03/25 20:36:30 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/26 14:38:39 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 typedef struct s_str
 {
-	char	*str;
-	int		len;
+	char			*str;
+	int				len;
 }	t_str;
 
 typedef struct s_env
 {
-	char			*key;
-	char			*value;
+	t_str			key;
+	t_str			value;
 	struct s_env	*next;
 }	t_env;
 
 typedef struct s_ctx
 {
-	int		fd_history;
-	t_env	*history;
+	int				fd_history;
+	t_env			*env;
+	t_env			*var;
 }	t_ctx;
