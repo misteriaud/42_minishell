7f2c0b93d87bf077b8d7adfad2c4856dafbbddff
Maxime Riaud <maxime.riaud@gmail.com>
2022-03-27 20:41:21 +0200

    ajout readline.h

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mriaud <mriaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 17:57:43 by mriaud            #+#    #+#             */
-/*   Updated: 2022/03/27 15:32:18 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/27 18:40:11 by mriaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 # include <term.h>
 # include <parsing.h>
 # include <utils.h>
+# include <readline.h>
 
 //# include <termios.h>
 # include <stdio.h>
