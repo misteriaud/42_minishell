ecbfa567175d692328e32f67fc5418ca8e97dea3
123Untitled <123untitled@pm.me>
2022-03-27 21:20:16 +0200

    init history

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mriaud <mriaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 17:57:54 by mriaud            #+#    #+#             */
-/*   Updated: 2022/03/27 18:17:44 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/27 19:20:31 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int	main(int ac, char **av, char **env)
 	t_ctx	ctx;
 	init_term(&ctx);
 	init_env(&ctx, env);
+	init_history(&ctx);
 	xfree_all();
 	return (0);
 }
