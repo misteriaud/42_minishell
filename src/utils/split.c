ee6c97eeb09876c59112b5697e3f2b48e87c61b2
123Untitled <123untitled@pm.me>
2022-03-27 21:19:43 +0200

    add inverted split

--- src/utils/split.c
+++ src/utils/split.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   split_arr.c                                        :+:      :+:    :+:   */
+/*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: artblin <artblin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 13:43:33 by artblin           #+#    #+#             */
-/*   Updated: 2022/03/27 17:17:19 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/27 21:14:50 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,12 +89,40 @@ t_err	split_arr(char ***array, const char *str, const char sep, const int alloc_
 	return (NO_ERROR);
 }
 
-t_err	split_lst(t_lst **head, const char *str,  const char sep, const int alloc_group)
+t_err	split_lst_inverted(t_lst **head, const char *str,  const char sep, const int alloc_group)
+{
+	t_lst	*elm;
+	int		byte;
+
+	*head = NULL;
+	while (*str)
+	{
+		while (*str == sep)
+			++str;
+		if (byte_count(str, &byte, sep))
+			break ;
+		if (xmalloc(&elm, sizeof(t_lst), alloc_group)
+			| xmalloc(&((elm)->data.str), (byte + 1), alloc_group))
+			return (MEMORY_ERROR);
+		(elm)->data.str[byte] = '\0';
+		(elm)->data.len = byte;
+		byte = 0;
+		while (*str && *str != sep)
+			(elm)->data.str[byte++] = *(str)++;
+		if (*head)
+			(*head)->prev = elm;
+		(elm)->next = *head;
+		*head = (elm);
+	}
+	return (NO_ERROR);
+}
+
+t_err	split_lst_normal(t_lst **head, const char *str,  const char sep, const int alloc_group)
 {
 	t_lst	**elm;
 	int		byte;
 
-	head = NULL;
+	*head = NULL;
 	elm = head;
 	while (*str)
 	{
