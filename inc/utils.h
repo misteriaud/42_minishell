8afe3ebcb792eac1699e98ccc47379216ffa55de
123Untitled <123untitled@pm.me>
2022-03-27 21:22:34 +0200

    add merge function

--- inc/utils.h
+++ inc/utils.h
@@ -6,7 +6,7 @@
 /*   By: artblin <artblin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 13:46:16 by artblin           #+#    #+#             */
-/*   Updated: 2022/03/27 18:09:36 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/27 21:15:19 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 
 # include <struct.h>
 
+t_err	merge(t_str *dest, char *str1, const char *str2);
 t_err	compare(const char *what, const char *with);
 t_err	split_arr(char ***array, const char *str, const char del, const int alloc_group);
-t_err	split_lst(t_lst **head, const char *str,  const char sep, const int alloc_group);
+t_err	split_lst_normal(t_lst **head, const char *str,  const char sep, const int alloc_group);
+t_err	split_lst_inverted(t_lst **head, const char *str,  const char sep, const int alloc_group);
 
 #endif
