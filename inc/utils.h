fbc4ec08d198485f758544fd3d8fb4b6cc961c57
123Untitled <123untitled@pm.me>
2022-03-27 18:20:56 +0200

    add compare function

--- inc/utils.h
+++ inc/utils.h
@@ -6,7 +6,7 @@
 /*   By: artblin <artblin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 13:46:16 by artblin           #+#    #+#             */
-/*   Updated: 2022/03/27 17:06:41 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/27 18:09:36 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <struct.h>
 
+t_err	compare(const char *what, const char *with);
 t_err	split_arr(char ***array, const char *str, const char del, const int alloc_group);
 t_err	split_lst(t_lst **head, const char *str,  const char sep, const int alloc_group);
 
