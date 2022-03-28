102fd1120cda1f33cdb25a1a63f1cca3ff431f3f
123Untitled <123untitled@pm.me>
2022-03-28 16:17:56 +0200

    update t_str arg + add exec_path function

--- inc/env.h
+++ inc/env.h
@@ -7,6 +7,8 @@
 
 t_err	init_env(t_ctx *ctx, char **env);
 t_err	print_env(t_ctx *ctx);
-t_err	get_env(t_ctx *ctx, const char *key, char **value);
+t_err	get_env(t_ctx *ctx, const char *key, t_str *value);
+t_err	refresh_paths(t_ctx *ctx);
+t_err	get_exec_path(t_ctx *ctx, t_str *exec);
 
 #endif
