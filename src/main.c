22a48dbcbc1606edbf8b1df7d4af4dc7da001bcb
123Untitled <123untitled@pm.me>
2022-03-28 16:21:23 +0200

    refresh path implementation

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mriaud <mriaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 17:57:54 by mriaud            #+#    #+#             */
-/*   Updated: 2022/03/27 19:20:31 by artblin          ###   ########.fr       */
+/*   Updated: 2022/03/28 16:12:38 by artblin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,12 @@ int	main(int ac, char **av, char **env)
 	init_term(&ctx);
 	init_env(&ctx, env);
 	init_history(&ctx);
+	refresh_paths(&ctx);
+
+	t_str	prog = {"nvim", 4};
+	get_exec_path(&ctx, &prog);
+	printf("prog -> %s\n", prog.str);
+
 	xfree_all();
 	return (0);
 }
