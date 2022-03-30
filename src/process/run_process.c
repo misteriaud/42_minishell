fabdd2e72961015f9da03b97d60737dde6f5b858
Maxime Riaud <maxime.riaud@gmail.com>
2022-03-30 12:41:24 +0200

    ajout de la prise en charge des built_in dans exec

--- src/process/run_process.c
+++ src/process/run_process.c
@@ -6,7 +6,7 @@
 /*   By: mriaud <mriaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 14:09:08 by mriaud            #+#    #+#             */
-/*   Updated: 2022/03/30 01:59:59 by mriaud           ###   ########.fr       */
+/*   Updated: 2022/03/30 12:40:55 by mriaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ int	execute_builtin(t_token *token, t_err *err)
 	return (NO_ERROR);
 }
 
-t_err	execute_bin(t_ctx *ctx, t_token *token, t_err *err)
+t_err	execute(t_ctx *ctx, t_token *token, t_err *err)
 {
 	pid_t	pid;
 	int		pfd[2];
 	char	**argv;
+	t_func	*built_func;
 
-	if (get_exec_path(ctx, &token->value))
+	built_func = search_built_in(ctx, token->value.str);
+	if (!built_func && get_exec_path(ctx, &token->value))
 		return (BIN_UNKNOWN);
-	if (get_exec_arg(&argv, token))
-		return (MEMORY_ERROR);
 	if (token->out)
 	{
 		if (token->out && pipe(pfd) == -1)
@@ -42,12 +42,19 @@ t_err	execute_bin(t_ctx *ctx, t_token *token, t_err *err)
 			close(pfd[1]); /* close write side */
 			dup2(pfd[0], 0); // connect read side with stdin
 			close(pfd[0]); // close read side
-			return (execute_bin(ctx, token->out, err));
+			return (execute(ctx, token->out, err));
+		}
+		else
+		{
+			close(pfd[0]);
+			dup2(pfd[1], 1); /* connect the write side with stdout */
+			close(pfd[1]); /* close the write side */
 		}
 	}
-	close(pfd[0]);
-	dup2(pfd[1], 1); /* connect the write side with stdout */
-	close(pfd[1]); /* close the write side */
+	if (!built_func && get_exec_arg(&argv, token))
+		return (MEMORY_ERROR);
+	if (built_func)
+		return (built_func(ctx, token->arg));
 	execve(token->value.str, argv, NULL);
 	return (NO_ERROR);
 }
@@ -61,5 +68,5 @@ t_err	run_process(t_ctx *ctx)
 	curr = ctx->parse_tree;
 	if (!curr)
 		return (PARSING_ERROR);
-	return (execute_bin(ctx, curr, &err));
+	return (execute(ctx, curr, &err));
 }
