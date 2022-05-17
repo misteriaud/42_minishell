2741747fccdcf6501a07d27799b2577ddfb2dbfa
Maxime Riaud (at 42) <mriaud@student.42.fr>
2022-05-17 12:50:09 +0200

    fix error: Conditional jump or move depends on uninitialised value(s)

--- src/utils/is_dir.c
+++ src/utils/is_dir.c
@@ -6,7 +6,7 @@
 /*   By: mriaud <mriaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 17:24:08 by mriaud            #+#    #+#             */
-/*   Updated: 2022/05/11 09:17:26 by mriaud           ###   ########.fr       */
+/*   Updated: 2022/05/17 12:45:07 by mriaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ t_err	is_dir(const char *path)
 {
 	struct stat	path_stat;
 
+	path_stat.st_mode = (mode_t){0};
 	stat(path, &path_stat);
 	if (S_ISDIR(path_stat.st_mode))
 		return (DIR_ERROR);
--- src/utils/is_reg.c
+++ src/utils/is_reg.c
@@ -6,7 +6,7 @@
 /*   By: mriaud <mriaud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 17:24:08 by mriaud            #+#    #+#             */
-/*   Updated: 2022/05/11 14:12:40 by mriaud           ###   ########.fr       */
+/*   Updated: 2022/05/17 12:43:16 by mriaud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ t_err	is_reg(const char *path)
 {
 	struct stat	path_stat;
 
+	path_stat.st_mode = (mode_t){0};
 	stat(path, &path_stat);
 	if (S_ISREG(path_stat.st_mode))
 		return (REG_ERROR);
